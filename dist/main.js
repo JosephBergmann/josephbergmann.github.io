a38ee17b0c4d67f012a28d29684a31db68355b44
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2023-01-16 21:49:48 -0500

    test

--- dist/main.js
+++ dist/main.js
@@ -1,60 +1,72 @@
-/*
- * ATTENTION: An "eval-source-map" devtool has been used.
- * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
- * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
- * or disable the default devtool with "devtool: false".
- * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
- */
-/******/ (function() { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
+// /*
+//  * ATTENTION: An "eval-source-map" devtool has been used.
+//  * This devtool is neither made for production nor for readable output files.
+//  * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+//  * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+//  * or disable the default devtool with "devtool: false".
+//  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+//  */
+// /******/ (function() { // webpackBootstrap
+// /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/index.js":
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-/***/ (function() {
+// /***/ "./src/index.js":
+// /*!**********************!*\
+//   !*** ./src/index.js ***!
+//   \**********************/
+// /***/ (function() {
 
-eval("//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvaW5kZXguanMuanMiLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9wcm9qZWN0Ly4vc3JjL2luZGV4LmpzP2I2MzUiXSwic291cmNlc0NvbnRlbnQiOlsiIl0sIm1hcHBpbmdzIjoiIn0=\n//# sourceURL=webpack-internal:///./src/index.js\n");
+// eval("//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvaW5kZXguanMuanMiLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9wcm9qZWN0Ly4vc3JjL2luZGV4LmpzP2I2MzUiXSwic291cmNlc0NvbnRlbnQiOlsiIl0sIm1hcHBpbmdzIjoiIn0=\n//# sourceURL=webpack-internal:///./src/index.js\n");
 
-/***/ }),
+// /***/ }),
 
-/***/ "./src/index.scss":
-/*!************************!*\
-  !*** ./src/index.scss ***!
-  \************************/
-/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+// /***/ "./src/index.scss":
+// /*!************************!*\
+//   !*** ./src/index.scss ***!
+//   \************************/
+// /***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvaW5kZXguc2Nzcy5qcyIsIm1hcHBpbmdzIjoiO0FBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9wcm9qZWN0Ly4vc3JjL2luZGV4LnNjc3M/OTc0NSJdLCJzb3VyY2VzQ29udGVudCI6WyIvLyBleHRyYWN0ZWQgYnkgbWluaS1jc3MtZXh0cmFjdC1wbHVnaW5cbmV4cG9ydCB7fTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/index.scss\n");
+// "use strict";
+// eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvaW5kZXguc2Nzcy5qcyIsIm1hcHBpbmdzIjoiO0FBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9wcm9qZWN0Ly4vc3JjL2luZGV4LnNjc3M/OTc0NSJdLCJzb3VyY2VzQ29udGVudCI6WyIvLyBleHRyYWN0ZWQgYnkgbWluaS1jc3MtZXh0cmFjdC1wbHVnaW5cbmV4cG9ydCB7fTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/index.scss\n");
 
-/***/ })
+// /***/ })
 
-/******/ 	});
-/************************************************************************/
-/******/ 	// The require scope
-/******/ 	var __webpack_require__ = {};
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	!function() {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = function(exports) {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	}();
-/******/ 	
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
-/******/ 	__webpack_modules__["./src/index.js"](0, {}, __webpack_require__);
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.scss"](0, __webpack_exports__, __webpack_require__);
-/******/ 	
-/******/ })()
-;
+// /******/ 	});
+// /************************************************************************/
+// /******/ 	// The require scope
+// /******/ 	var __webpack_require__ = {};
+// /******/ 	
+// /************************************************************************/
+// /******/ 	/* webpack/runtime/make namespace object */
+// /******/ 	!function() {
+// /******/ 		// define __esModule on exports
+// /******/ 		__webpack_require__.r = function(exports) {
+// /******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+// /******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+// /******/ 			}
+// /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+// /******/ 		};
+// /******/ 	}();
+// /******/ 	
+// /************************************************************************/
+// /******/ 	
+// /******/ 	// startup
+// /******/ 	// Load entry module and return exports
+// /******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
+// /******/ 	__webpack_modules__["./src/index.js"](0, {}, __webpack_require__);
+// /******/ 	var __webpack_exports__ = {};
+// /******/ 	__webpack_modules__["./src/index.scss"](0, __webpack_exports__, __webpack_require__);
+// /******/ 	
+// /******/ })()
+// ;
+$.ajax({
+  url: "https://en.wikipedia.org/w/api.php?action=query&format=json&list=categorymembers&cmtitle=Category:Language_families&cmlimit=500",
+  type: "GET",
+  dataType: "json",
+  success: function (data) {
+      var families = data.query.categorymembers;
+      for (var i = 0; i < families.length; i++) {
+          var family = families[i];
+          console.log(family.title + ": " + family.pageid);
+      }
+  }
+});
