b80a13051c1cdc0f4c29d4b02d8a3c2a2f797a6e
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2023-01-17 01:46:01 -0500

    got api to work

--- dist/main.js
+++ dist/main.js
@@ -59,14 +59,22 @@
 // /******/ })()
 // ;
 $.ajax({
-  url: "https://en.wikipedia.org/w/api.php?action=query&format=json&list=categorymembers&cmtitle=Category:Language_families&cmlimit=500",
+  url: "https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=query&format=json&list=categorymembers&cmtitle=Category:Language_families&cmlimit=500",
   type: "GET",
   dataType: "json",
   success: function (data) {
       var families = data.query.categorymembers;
-      for (var i = 0; i < families.length; i++) {
-          var family = families[i];
+      for (let i = 0; i < families.length; i++) {
+          let family = families[i];
+          let newLink= $('<a>',{
+            href: 'https://en.wikipedia.org/wiki/' + family.title,
+            text: family.title
+          });
           console.log(family.title + ": " + family.pageid);
+          $('#families-list').append($('<li>').append(newLink));
       }
+  },
+  error: function(xhr, status, error) {
+      console.log(xhr.responseText);
   }
 });
